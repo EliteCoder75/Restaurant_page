c752d9b65648699823619b182a467208822bd056
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2025-02-10 23:36:55 +0100

    add seperate modules for each tab

--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
+
+import {component} from './page-load.js';
+import {menu} from './menu.js';
+import {about} from './about.js';
+
 console.log("salem");
-alert("salemlem")
+
+const bts = document.querySelectorAll('button');
+
+
+/*bts.forEach(function(elem) {
+  elem.addEventListener("click", function() {
+    document.body.appendChild(component());
+  });
+});
+*/
+//document.body.appendChild(component());
+//document.body.appendChild(menu());
+//document.body.appendChild(about());
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 
+
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
@@ -20,4 +21,17 @@ module.exports = {
       template: "./src/template.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      
+    ],
+  },
 };
