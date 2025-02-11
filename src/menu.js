bb1bde52d7f22a9e21dcc073bb3580ba64eb0931
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2025-02-11 18:33:59 +0100

    update the menu page

--- src/menu.js
+++ src/menu.js
@@ -2,14 +2,18 @@
 import img from './images/menu.jpeg';
 export function menu() {
     const contentElement = document.querySelector("#content");
-    const element = document.createElement('div');
     const paragraph = document.createElement('p');
-    paragraph.textContent= "crousti begin road";
+    contentElement.style = 'display: flex';
     const myIcon = new Image();
     myIcon.src = img;
+    myIcon.style.alignSelf = 'center';
+    myIcon.style.width = '80%';
+    myIcon.style.height = '40%';
+    myIcon.style.margin = 'auto';
+    
+
     contentElement.appendChild(myIcon);
-    contentElement.appendChild(paragraph);
-    return element;
+    return contentElement;
 }
 
   
