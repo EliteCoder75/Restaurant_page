8e20a1538f06920492c8340ebe28c9aeebc07252
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2025-02-11 12:49:44 +0100

    implementing switch tab logic between different navigation bars

--- src/menu.js
+++ src/menu.js
@@ -1,4 +1,6 @@
-import img from './images/menu.jpeg';
+/*import img from './images/menu.jpeg';
+
+
 export function menu() {  
     const contentElement = document.querySelector("#content");
     const element = document.createElement('div');
@@ -15,3 +17,26 @@ export function menu() {
    return element;
   }
 
+  */
+
+  import img from './images/menu.jpeg';
+  export function menu() {
+      const contentElement = document.querySelector("#content");
+      const element = document.createElement('div');
+      const paragraph = document.createElement('p');
+      paragraph.textContent= "crousti begin road";
+      // Lodash, now imported by this script
+      //element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+      //element.classList.add('hello');
+     // Add the image to our existing div.
+  
+     const myIcon = new Image();
+     myIcon.src = img;
+     contentElement.appendChild(myIcon);
+     contentElement.appendChild(paragraph);
+      
+      return element;
+    }
+  
+  
+    
