fc95f07ba8ea2756e91f7d01d52d2d0911c61e48
EliteCoder75 <e.gueddouda.inf@lagh-univ.dz>
2025-02-12 14:35:46 +0100

    create clear function to avoid repating code in the index.js file

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
+import "./style.css";
+
 import {component} from './page-load.js';
 import {menu} from './menu.js';
 import {about} from './about.js';
+
 
 const home = document.querySelector("button#home");
 const menuu = document.querySelector("button#menuu");
 const aboutt = document.querySelector("button#aboutt");
 
+
+function clearPage(e) {
+  Array.from(document.querySelector("#content").childNodes).forEach((child) => child.remove());
+}
 
 (function default_load()
     {
@@ -17,23 +24,20 @@ const aboutt = document.querySelector("button#aboutt");
 {
   home.addEventListener('click', (event)=>
     {
-      event.preventDefault();
-      document.querySelector("#content").innerHTML = ""; 
+      clearPage();
       document.body.appendChild(component()); 
     }
   );
 
   menuu.addEventListener('click', (event)=>
     {
-      event.preventDefault();
-      document.querySelector("#content").innerHTML = ""; 
+      clearPage();
       document.body.appendChild(menu()); 
     });
 
   aboutt.addEventListener('click', (event)=>
     {
-      event.preventDefault();
-      document.querySelector("#content").innerHTML = ""; 
+      clearPage(); 
       document.body.appendChild(about()); 
     }
   );
